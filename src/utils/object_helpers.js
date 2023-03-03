60b6d365e5e990e179822193824af82bab7caec5
Josh Smith <jmsmith1018@gmail.com>
2023-03-02 21:35:19 -0700

    chore: add object utility helper methods

---
+++ src/utils/object_helpers.js
@@ -0,0 +1,22 @@
+export const objectWithDefault = (defaultValue) => {
+  return (obj) => {
+    return Object.keys(obj).reduce((defObj, key) => {
+      defObj[key] = obj[key] ?? defaultValue;
+      return defObj;
+    }, {})
+  }
+}
+
+export const isEmpty = (obj) => {
+  return Object.values(obj).every((v) => !v);
+}
+
+export const objFromForm = (form) => {
+  const formData = new FormData(form);
+
+  const object = {};
+  for (const [key, value] of formData.entries()) {
+    object[key] = value;
+  }
+  return object;
+}
