7d9df65a7e2a2238ad6fee35c14e1cc5e2b77c83
Josh Smith <jmsmith1018@gmail.com>
2023-03-02 22:48:15 -0700

    feat: allow editing of general information

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -3,23 +3,39 @@ import React, { Component } from "react";
 import { isEmpty } from "../utils/object_helpers";
 
 class GeneralInfo extends Component {
+  createButton(text) {
+    return (
+      <button
+        className="border border-black"
+        onClick={this.props.clickHandler}
+        data-component="Info"
+      >
+        {text}
+      </button>
+    )
+  }
+
   render() {
     if (isEmpty(this.props.info)) {
       return (
         <div>
           <p>You haven't filled out this information yet</p>
-          <button
-            className="border border-black"
-            onClick={this.props.clickHandler}
-            data-component="Info"
-          >
-            Add General Information
-          </button>
+          {this.createButton("Add your info")}
         </div>
       )
     }
 
-    return <div>Hello from gen info.</div>;
+    const { name, address, email, phoneNumber } = this.props.info;
+
+    return (
+      <div>
+        <p>Name: {name}</p>
+        <p>City: {address}</p>
+        <p>Email: {email}</p>
+        <p>Phone: {phoneNumber}</p>
+        {this.createButton("Edit your info")}
+      </div>
+    )
   }
 }
 
