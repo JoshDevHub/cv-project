19d1a71e7b4e93070dc5455e6e8895ea0b8105ed
Josh Smith <jmsmith1018@gmail.com>
2023-03-02 21:37:22 -0700

    feat: hook up components for general info

--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
 import "./index.css";
 import React, { Component } from "react";
 
+import GeneralInfo from "./components/GeneralInfo";
+import GeneralInfoForm from "./components/GeneralInfoForm";
+
+import FORM_STATES from "./utils/form_states";
+
+import GeneralInfoModel from "./models/general_info_model";
+
+import { objFromForm } from "./utils/object_helpers";
+
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      genInfo: GeneralInfoModel(),
+      formComponent: FORM_STATES.None,
+    }
+  }
+
+  isFormFor(state) {
+    return this.state.formComponent === FORM_STATES[state];
+  }
+
+  handleOpenFormClick = (event) => {
+    const componentIdentifier = event.target.getAttribute("data-component");
+    this.setState({
+      formComponent: FORM_STATES[componentIdentifier]
+    })
+  }
+
+  onSubmitInfo = (event) => {
+    event.preventDefault();
+
+    const data = objFromForm(event.target);
+    this.setState({
+      genInfo: GeneralInfoModel(data),
+      formComponent: FORM_STATES.None
+    })
+  }
+
   render() {
-    return <div className="text-2xl">Hello World</div>;
+    const { genInfo } = this.state
+
+    return (
+      <div>
+        {this.isFormFor("Info")
+          ? <GeneralInfoForm info={genInfo} handleSubmit={this.onSubmitInfo} />
+          : <GeneralInfo info={genInfo} clickHandler={this.handleOpenFormClick} />
+        }
+      </div>
+    )
   }
 }
 
