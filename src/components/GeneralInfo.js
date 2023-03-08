c9b77dd1d104772173fe8853f8e75126d36eac9d
Josh Smith <jmsmith1018@gmail.com>
2023-03-07 21:18:37 -0700

    refactor: use `Button` component in place of plain HTML

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -5,20 +5,16 @@ import Button from "./Button";
 import { isEmpty } from "../utils/object_helpers";
 
 class GeneralInfo extends Component {
-  createButton(text) {
-    return (
-      <button className="border border-black" onClick={this.props.clickHandler}>
-        {text}
-      </button>
-    );
-  }
-
   render() {
     if (isEmpty(this.props.info)) {
       return (
         <div>
           <p>You haven't filled out this information yet</p>
-          {this.createButton("Add your info")}
+          <Button
+            type="button"
+            handler={this.props.clickHandler}
+            text="Add Info"
+          />
         </div>
       );
     }
