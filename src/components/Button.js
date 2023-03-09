453d5a6f29dcf78c57480a38fa8f9abc4fea5d44
Josh Smith <jmsmith1018@gmail.com>
2023-03-08 21:09:26 -0700

    feat: support different styling flavors for buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
 
+const STYLES_MAP = new Map([
+  ["primary", "bg-sky-800 border-sky-800 hover:text-sky-800"],
+  ["alert", "bg-rose-700 border-rose-700 hover:text-rose-700"],
+]);
+
 class Button extends Component {
   render() {
-    const { type, text, handler, value } = this.props;
+    const { type, text, handler, value, styles } = this.props;
+    const colors = STYLES_MAP.get(styles || "primary");
 
     return (
       <button
@@ -10,8 +16,8 @@ class Button extends Component {
         onClick={handler}
         {...(handler && { onClick: handler })}
         value={value ?? ""}
-        className="rounded-md border-2 border-sky-800 bg-sky-800 p-1 font-bold
-                   text-white hover:bg-transparent hover:text-sky-800"
+        className={`rounded-md border-2 p-1 font-bold text-white
+                   hover:bg-transparent ${colors}`}
       >
         {text}
       </button>
