78fdcc90516dfbad2526700220fd852043e3305c
Josh Smith <jmsmith1018@gmail.com>
2023-03-08 21:34:44 -0700

    feat: add header to the site

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./index.css";
 import React, { Component } from "react";
 
+import Header from "./components/Header";
 import GeneralInformation from "./components/GeneralInformation";
 import Skills from "./components/Skills";
 
@@ -8,7 +9,8 @@ class App extends Component {
   render() {
     return (
       <div className="min-h-screen bg-slate-900 py-8">
-        <div className="mx-auto max-w-4xl bg-white">
+        <Header />
+        <div className="mx-auto max-w-4xl rounded-xl bg-white">
           <GeneralInformation />
           <Skills />
         </div>
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="mb-8 flex justify-center">
+        <h1 className="text-3xl font-bold text-white">CV Application</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
