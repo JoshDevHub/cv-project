a798973001ac687a0c579f70d9eb18d6521319ef
Josh Smith <jmsmith1018@gmail.com>
2023-03-15 20:13:57 -0600

    refactor: `WorkExperience` component to functional style

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
 import Button from "./Button";
 import Work from "./Work";
@@ -17,91 +17,67 @@ const EmptyWorkExperience = () => {
   };
 };
 
-class WorkExperience extends Component {
-  constructor(props) {
-    super(props);
+const WorkExperience = () => {
+  const [experiences, setExperiences] = useState([]);
+  const [workItem, setWorkItem] = useState(EmptyWorkExperience());
+  const [showForm, setShowForm] = useState(false);
 
-    this.state = {
-      experiences: [],
-      workExperience: EmptyWorkExperience(),
-      showForm: false,
-    };
-  }
+  const toggleForm = () => setShowForm(!showForm);
 
-  toggleForm = () => {
-    this.setState({
-      showForm: !this.state.showForm,
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setWorkItem({
+      ...workItem,
+      [name]: value,
     });
   };
 
-  handleChange = (event) => {
-    const prop = event.target.id;
-    this.setState({
-      workExperience: {
-        ...this.state.workExperience,
-        [prop]: event.target.value,
-      },
-    });
-  };
-
-  handleRemove = (id) => {
+  const handleRemove = (id) => {
     return () => {
-      this.setState({
-        experiences: this.state.experiences.filter((exp) => exp.id !== id),
-      });
+      setExperiences(experiences.filter((exp) => exp.id !== id));
     };
   };
 
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
 
-    this.setState({
-      experiences: [...this.state.experiences, this.state.workExperience],
-      workExperience: EmptyWorkExperience(),
-      showForm: false,
-    });
+    setExperiences([...experiences, workItem]);
+    setWorkItem(EmptyWorkExperience());
+    setShowForm(false);
   };
 
-  listExperiences() {
-    if (this.state.experiences.length === 0) {
+  const listExperiences = () => {
+    if (experiences.length === 0) {
       return <p className="mb-4">You haven't added any work experience yet</p>;
     }
 
     return (
       <ul>
-        {this.state.experiences.map(({ id, ...experience }) => {
+        {experiences.map(({ id, ...experience }) => {
           return (
-            <Work
-              key={id}
-              data={experience}
-              removeHandler={this.handleRemove(id)}
-            />
+            <Work key={id} data={experience} removeHandler={handleRemove(id)} />
           );
         })}
       </ul>
     );
-  }
+  };
 
-  render() {
-    return (
-      <div className="px-8 py-4">
-        <h2 className="mb-4 text-2xl font-bold text-sky-800">
-          Work Experience
-        </h2>
-        {this.listExperiences()}
-        {this.state.showForm ? (
-          <WorkForm
-            experience={this.state.workExperience}
-            handleChange={this.handleChange}
-            handleSubmit={this.handleSubmit}
-            closeForm={this.toggleForm}
-          />
-        ) : (
-          <Button type="button" text="Add Work" handler={this.toggleForm} />
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="px-8 py-4">
+      <h2 className="mb-4 text-2xl font-bold text-sky-800">Work Experience</h2>
+      {listExperiences()}
+      {showForm ? (
+        <WorkForm
+          experience={workItem}
+          handleChange={handleChange}
+          handleSubmit={handleSubmit}
+          closeForm={toggleForm}
+        />
+      ) : (
+        <Button type="button" text="Add Work" handler={toggleForm} />
+      )}
+    </div>
+  );
+};
 
 export default WorkExperience;
