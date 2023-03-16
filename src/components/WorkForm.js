9358ed754fb326f3017299476cbb6fa9335b819c
Josh Smith <jmsmith1018@gmail.com>
2023-03-15 21:38:30 -0600

    refactor: move state for individual item to `Form` level
    This was a change with the intention of implementing edit/update
    features. The form is more reusable for creating/updating if it
    controls the input states.

--- src/components/WorkForm.js
+++ src/components/WorkForm.js
@@ -1,31 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Button from "./Button";
 import Input from "./Input";
 
+import uniqid from "uniqid";
+
+const EmptyWorkExperience = () => {
+  return {
+    id: uniqid(),
+    startDate: "",
+    endDate: "",
+    companyName: "",
+    jobTitle: "",
+    responsibilities: "",
+  };
+};
+
 const WorkForm = (props) => {
-  const { startDate, endDate, jobTitle, companyName, responsibilities } =
-    props.experience;
+  const [workItem, setWorkItem] = useState(props.data ?? EmptyWorkExperience);
+  /* const { startDate, endDate, jobTitle, companyName, responsibilities } = */
+  /*   props.experience; */
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setWorkItem({
+      ...workItem,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(workItem);
+    props.submitAction(workItem);
+  };
 
   return (
-    <form
-      onSubmit={props.handleSubmit}
-      className="grid grid-cols-3 gap-y-2 gap-x-8"
-    >
+    <form onSubmit={handleSubmit} className="grid grid-cols-3 gap-y-2 gap-x-8">
       <div className="col-span-2 flex flex-col gap-0.5">
         <label htmlFor="companyName">Company Name</label>
         <Input
           type="text"
           id="companyName"
-          value={companyName}
-          handler={props.handleChange}
+          value={workItem.companyName}
+          handler={handleChange}
         />
         <label htmlFor="jobTitle col-span-2">Job Title</label>
         <Input
           type="text"
           id="jobTitle"
-          value={jobTitle}
-          handler={props.handleChange}
+          value={workItem.jobTitle}
+          handler={handleChange}
         />
       </div>
       <div className="flex flex-col gap-0.5">
@@ -33,15 +58,15 @@ const WorkForm = (props) => {
         <Input
           type="text"
           id="startDate"
-          value={startDate}
-          handler={props.handleChange}
+          value={workItem.startDate}
+          handler={handleChange}
         />
         <label htmlFor="endDate">End Date</label>
         <Input
           type="text"
           id="endDate"
-          value={endDate}
-          handler={props.handleChange}
+          value={workItem.endDate}
+          handler={handleChange}
         />
       </div>
       <div className="col-span-3 mb-2 flex flex-col">
@@ -49,15 +74,16 @@ const WorkForm = (props) => {
         <textarea
           id="responsibilities"
           name="responsibilities"
-          value={responsibilities}
-          onChange={props.handleChange}
+          value={workItem.responsibilities}
+          onChange={handleChange}
           placeholder="List some of your responsibilities ..."
           className="rounded-md border px-2 py-1"
           rows="5"
         />
       </div>
       <div className="flex gap-4">
-        <Button type="submit" text="Add Experience" />
+        {/* <Button type="submit" text="Add Experience" /> */}
+        {props.submitButton}
         <Button
           type="button"
           text="Cancel"
